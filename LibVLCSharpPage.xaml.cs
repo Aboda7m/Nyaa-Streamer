b1943646d50416388a217e197b09777c0d429fa8
BuildTools <unconfigured@null.spigotmc.org>
2024-09-09 19:53:37 +0300

    custom control bar myself? ??? ??

--- LibVLCSharpPage.xaml.cs
+++ LibVLCSharpPage.xaml.cs
@@ -3,11 +3,17 @@
 using LibVLCSharp.Shared;
 using LibVLCSharp.MAUI;
 using System.Diagnostics;
+using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
 
 namespace Nyaa_Streamer
 {
     public partial class LibVLCSharpPage : ContentPage
     {
+        private bool _isPlaying = true; // Assume video starts playing
+        private const int AnimationDuration = 500; // Duration for fade-in/out animation
+        private const int ControlBarTimeout = 3000; // Time in milliseconds to hide the control bar
+
         public LibVLCSharpPage()
         {
             InitializeComponent();
@@ -29,6 +35,43 @@ namespace Nyaa_Streamer
         private void VideoView_MediaPlayerChanged(object sender, MediaPlayerChangedEventArgs e)
         {
             ((MainViewModel)BindingContext).OnVideoViewInitialized();
+        }
+
+        private void OnPlayPauseClicked(object sender, EventArgs e)
+        {
+            _isPlaying = !_isPlaying;
+
+            if (_isPlaying)
+            {
+                VideoView.MediaPlayer.Play();
+                ((Button)sender).Text = "Pause";
+            }
+            else
+            {
+                VideoView.MediaPlayer.Pause();
+                ((Button)sender).Text = "Play";
+            }
+
+            // Hide the control bar after a delay
+            HideControlBarWithDelay();
+        }
+
+        private async void OnScreenTapped(object sender, EventArgs e)
+        {
+            // Show the control bar
+            await ControlBar.FadeTo(1, AnimationDuration);
+
+            // Hide the control bar after a delay
+            HideControlBarWithDelay();
+        }
+
+        private async void HideControlBarWithDelay()
+        {
+            // Wait for the specified timeout
+            await Task.Delay(ControlBarTimeout);
+
+            // Fade out the control bar
+            await ControlBar.FadeTo(0, AnimationDuration);
         }
     }
 }
